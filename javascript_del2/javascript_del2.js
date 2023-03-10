9f2cdad2afc486cc5e638278c0a9f6c375a0c957
LauritsNS20 <113506257+LauritsNS20@users.noreply.github.com>
2023-03-10 10:10:50 +0100

    Add files via upload

---
+++ javascript_del2/javascript_del2.js
@@ -0,0 +1,49 @@
+var tekst = "Hej verden";
+alert (tekst);
+
+var start = "Hej ";
+var slut = "verden";
+var samlet = start + slut;
+document.write(samlet)
+
+var tal1 = 2;
+var tal2 = 8;
+var tallet = tal1 + tal2;
+document.write(tallet)
+
+var start2 = "dette er en tekst";
+var slut2 = 1337;
+var samlet2 = start2 + slut2;
+alert (samlet2);
+
+var tal3 = 15;
+var tal4 = 5;
+document.write(tal3 + tal4);
+document.write(tal3 - tal4);
+document.write(tal3 * tal4);
+document.write(tal3 / tal4);
+
+var skriv = prompt ("skriv et tal");
+alert (skriv)
+
+if(isNaN(prin)) {
+    alert(prin + " is not a number");
+} else {
+    alert(prin + " is not a number");
+}
+
+var tal = prompt("skriv et tal");
+while(isNaN(tal)) {
+    tal = prompt("skriv et tal");
+}
+alert(tal + " er et tal");
+
+var u = 2;
+var k = "1";
+var l = false;
+alert(typeof(u));
+alert(typeof(k));
+alert(typeof(l));
+
+
+
